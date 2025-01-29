7765855c6cc6b4fee1374e9c47a8c4bf97529301
Shehta2000 <mohamedshehta819@gmail.com>
2025-01-30 01:28:42 +0200

    - create theming ColorsManager & TextStyles - create generate route For Navigation . - Create Onboarding Screen - made a file for doc app client . - adding extention for navigation
    -

---
+++ lib/core/routing/routes.dart
@@ -0,0 +1,5 @@
+class Routes {
+  static const String onBoardingScreen = '/onboardingScreen';
+  static const String homeScreen = '/homeScreen';
+   static const String loginScreen = '/homeScreen';
+}
---
+++ lib/core/theming/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class ColorManager {
+
+  static const Color mainBlue = Color(0xFF247CFF);
+  static const Color grey = Color(0xFF757575);
+  static const Color white = Color(0xFFFFFFFF);
+
+}
