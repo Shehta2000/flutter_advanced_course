e20acd2f03c9490cfabd84b6fa6fa80ce0ce56f7
Shehta2000 <mohamedshehta819@gmail.com>
2025-02-16 00:28:23 +0200

    Finish

--- lib/core/networking/api_service.dart
+++ lib/core/networking/api_service.dart
@@ -1,6 +1,6 @@
 import 'package:dio/dio.dart';
-import 'package:flutter_advanced_course/features/home/data/models/sign_up_request_body.dart';
-import 'package:flutter_advanced_course/features/home/data/models/sign_up_response_body.dart';
+import 'package:flutter_advanced_course/features/sign_up/data/models/sign_up_request_body.dart';
+import 'package:flutter_advanced_course/features/sign_up/data/models/sign_up_response_body.dart';
 import 'package:retrofit/retrofit.dart';
 import '../../features/login/data/models/login_request_body.dart';
 import '../../features/login/data/models/login_response.dart';
